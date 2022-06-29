f1aa10985259e22807d44cf379321a562cd7384d
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-28 21:57:00 -0300

    Create trackpad.java

---
+++ Objects Strings Ints Booleans/trackpad.java
@@ -0,0 +1,10 @@
+package mousecompany;
+
+public class trackpad extends Mouse {
+    boolean trackpad = true;
+    boolean ambidextrous = true;
+
+    public static void move() {
+        System.out.println("This track pad slides to move!!");
+    }
+}
