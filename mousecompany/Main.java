31b46b4105354a5c5ef73206e363b39ea1cf9a81
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-28 22:08:28 -0300

    Update Main.java

--- mousecompany/Main.java
+++ mousecompany/Main.java
@@ -5,7 +5,7 @@ public class Main {
         mouse1 m1 = new mouse1();
         mouse2 m2 = new mouse2();
         mouse3 m3 = new mouse3();
-        trackpad track = new Trackpad();
+        trackpad track = new trackpad();
         m1.ScrollUP();
         m1.rightClick();
         m2.ScrollDOWN();
@@ -13,7 +13,12 @@ public class Main {
         m2.connect();
         System.out.println("Is the mouse3 ambidextrous?");
         System.out.println(m3.ambidextrous);
+        System.out.println("How does the trackpad move?");
         trackpad.move();
+        System.out.println("Is the trackpad ambidextrous?");
         System.out.println(track.ambidextrous);
+        System.out.println("What is the trackpad's size ? ");
+        System.out.println(track.size + track.sizetipe);
+
     }
 }
