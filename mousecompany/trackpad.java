e1944e99dd981b7e3f233e3c485c423d959c2a7d
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-28 22:08:53 -0300

    Update trackpad.java

--- mousecompany/trackpad.java
+++ mousecompany/trackpad.java
@@ -3,6 +3,8 @@ package mousecompany;
 public class trackpad extends Mouse {
     boolean trackpad = true;
     boolean ambidextrous = true;
+    int size = 7;
+    String sizetipe = "cm";
 
     public static void move() {
         System.out.println("This track pad slides to move!!");
