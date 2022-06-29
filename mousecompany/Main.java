025eb6302dcd32555305ab0110ef73a0b3da93a6
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-28 22:01:06 -0300

    Update Main.java

--- mousecompany/Main.java
+++ mousecompany/Main.java
@@ -5,11 +5,14 @@ public class Main {
         mouse1 m1 = new mouse1();
         mouse2 m2 = new mouse2();
         mouse3 m3 = new mouse3();
+        trackpad track = new Trackpad();
         m1.ScrollUP();
         m1.rightClick();
         m2.ScrollDOWN();
         m2.leftClick();
         m2.connect();
         System.out.println(m3.ambidextrous);
+        trackpad.move();
+        
     }
 }
