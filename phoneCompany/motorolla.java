3ffe55f6df6ace4b33e20ec4f7bfef4abe2eead2
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-08 13:15:20 -0300

    Update motorolla.java

--- phoneCompany/motorolla.java
+++ phoneCompany/motorolla.java
@@ -2,7 +2,7 @@ package phoneCompany;
 
 public class motorolla extends Phone {
     int size = 5;
-    
+    String color = "Red";
     String sizetipe = "cm";
     
     public static void turnON() {
