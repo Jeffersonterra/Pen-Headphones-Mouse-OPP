f93fdd936dd368b44f1bfbf92ac6bca0adaad519
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-08 13:14:49 -0300

    Update motorolla.java

--- phoneCompany/motorolla.java
+++ phoneCompany/motorolla.java
@@ -3,6 +3,8 @@ package phoneCompany;
 public class motorolla extends Phone {
     int size = 5;
     
+    String sizetipe = "cm";
+    
     public static void turnON() {
         System.out.println("Hello motto!!");
     }
