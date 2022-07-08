52c4c9a5ad3c6449be9d822a86d5498c3f7a2a57
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-08 13:28:20 -0300

    Update iphone.java

--- phoneCompany/iphone.java
+++ phoneCompany/iphone.java
@@ -2,6 +2,6 @@ package phoneCompany;
 
 public class iphone extends Phone {
     boolean touchscreen = true;
-    double screensize = 5;
+    double screensize = 5.8;
     String sizetipe = "cm";
 }
