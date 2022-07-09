14875276763d8d254c979c4afd1eb6518ce6f2f0
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-09 20:52:13 -0300

    Update motorolla.java

--- phoneCompany/motorolla.java
+++ phoneCompany/motorolla.java
@@ -8,4 +8,11 @@ public class motorolla extends Phone {
     public static void turnON() {
         System.out.println("Hello motto!!");
     }
+     public static void Whatfone() {
+
+        System.out.println("Iam a Motorolla!");
+
+       
+
+    }
 }
