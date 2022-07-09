61d670ffdac53554c0e2f82756186ad06bc0af47
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-09 20:51:47 -0300

    Update iphone.java

--- phoneCompany/iphone.java
+++ phoneCompany/iphone.java
@@ -4,4 +4,12 @@ public class iphone extends Phone {
     boolean touchscreen = true;
     double screensize = 5.8;
     String sizetipe = "cm";
+    
+    public static void Whatfone() {
+
+        System.out.println("Iam a Iphone!");
+
+       
+
+    }
 }
