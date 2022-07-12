dc9c8a5bafa888101eeca74d9395059a8a9ca075
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-11 21:25:23 -0300

    Update iphone.java

--- phoneCompany/iphone.java
+++ phoneCompany/iphone.java
@@ -4,7 +4,9 @@ public class iphone extends Phone {
     boolean touchscreen = true;
     double screensize = 5.8;
     String sizetipe = "cm";
-    
+    Boolean fulltouch = true;
+
+
     public static void Whatfone() {
 
         System.out.println("I am a Iphone!");
