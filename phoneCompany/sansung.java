64dc0d5e50bd41cf9a6af8006b524068bde5666e
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-11 21:26:26 -0300

    Update sansung.java

--- phoneCompany/sansung.java
+++ phoneCompany/sansung.java
@@ -4,6 +4,8 @@ public class sansung extends Phone {
 int screensize = 7;
 String sizetipe = "cm";
 String color = "black";
+Boolean fulltouch = true;
+
    public static void Whatfone() {
 
         System.out.println("I am a Smart fone!");
